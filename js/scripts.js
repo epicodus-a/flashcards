4e7c16af48d94039b57a00c99cc1df61f4e08fd5
Adriana C and adrian messado <adriana@>
2018-03-20 14:16:38 -0700

    make index.html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$().ready(function(){
+  $(".row1 div:nth-child(1)").click(function(){
+    // $(.row1 div:nth-child(1)).text("expaninged well.");
+    $(".row1 div:nth-child(1)").text("Hello world!");
+  });
+})
