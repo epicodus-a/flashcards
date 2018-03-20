ed9ba212aed62d504f2ae00695d9ff320c769326
Adriana C and adrian messado <adriana@>
2018-03-20 14:38:09 -0700

    add jq to get anwers

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
 $().ready(function(){
   $(".row1 div:nth-child(1)").click(function(){
-    // $(.row1 div:nth-child(1)).text("expaninged well.");
-    $(".row1 div:nth-child(1)").text("Hello world!");
+    $(".row1 div:nth-child(1)").text("A script language to manipulate DOM");
   });
+
+  $(".row1 div:nth-child(2)").click(function(){
+    $(".row1 div:nth-child(2)").text("A special charactor indates an action to be performed.");
+  });
+
+  $(".row1 div:nth-child(3)").click(function(){
+    $(".row1 div:nth-child(3)").text("Containers used to store information.");
+  });
+
+  $(".row2 div:nth-child(1)").click(function(){
+    $(".row2 div:nth-child(1)").text("lowerCamelCase.");
+  });
+
+  $(".row2 div:nth-child(2)").click(function(){
+    $(".row2 div:nth-child(2)").text("A block of codes performs actions and return a result.");
+  });
+
+  $(".row2 div:nth-child(3)").click(function(){
+    $(".row2 div:nth-child(3)").text("An action run on a piece of data.");
+  });
+
+  $(".row3 div:nth-child(1)").click(function(){
+    $(".row3 div:nth-child(1)").text("Provides method with additional information to help it perform its actions.");
+  });
+
+  $(".row3 div:nth-child(2)").click(function(){
+    $(".row3 div:nth-child(2)").text("A named varable passed into a function.");
+  });
+
+  $(".row3 div:nth-child(3)").click(function(){
+    $(".row3 div:nth-child(3)").text("It is a method response.");
+  });
+
 })
